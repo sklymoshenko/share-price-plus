c1ed6a9ca3db52695e1d6e7c918b8f702201b66c
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-02-13 17:42:14 +0100

    changing event prices animation

--- src/components/SpEvent/SpEventEdit/SpEventEditMainInfo.vue
+++ src/components/SpEvent/SpEventEdit/SpEventEditMainInfo.vue
@@ -3,19 +3,19 @@
     <div class="col">
       <div class="row items-end justify-between">
         <div class="sp-event-name text-h6 q-mr-sm">{{ name }}:</div>
-        <div class="sp-event-price text-h4">{{ totalPrice }}</div>
+        <div class="sp-event-price text-h4" :class="classPrice">{{ totalPrice }}</div>
       </div>
     </div>
     <div class="col">
       <div class="row items-end justify-between">
         <div class="sp-event-name text-h6 q-mr-sm">Each:</div>
-        <div class="sp-event-price text-h4">{{ each }}</div>
+        <div class="sp-event-price text-h4" :class="classEach">{{ each }}</div>
       </div>
     </div>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, ref, toRefs, watch } from "vue";
 
 // Types
 import { ISpEvent } from "@/types/entities/event";
@@ -35,8 +35,41 @@ export default defineComponent({
       type: String as PropType<ISpEvent["name"]>
     }
   },
-  setup() {}
+  setup(props) {
+    const { totalPrice, each } = toRefs(props);
+    let timerPrice = ref(null);
+    let timerEach = ref(null);
+    const changingTextClass = "text-accent";
+    const classPrice = ref("");
+    const classEach = ref("");
+
+    watch(totalPrice, (newVal, oldVal) => {
+      if (oldVal === newVal) return;
+      if (timerPrice.value) clearTimeout(timerPrice.value);
+
+      classPrice.value = changingTextClass;
+      setTimeout(() => {
+        classPrice.value = "";
+      }, 1000);
+    });
+
+    watch(each, (newVal, oldVal) => {
+      if (oldVal === newVal) return;
+      if (timerEach.value) clearTimeout(timerEach.value);
+
+      classEach.value = changingTextClass;
+      setTimeout(() => {
+        classEach.value = "";
+      }, 1000);
+    });
+
+    return { classPrice, classEach };
+  }
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.sp-event-price {
+  transition: text 0.7s ease-in-out;
+}
+</style>
