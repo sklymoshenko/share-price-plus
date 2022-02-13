ab0fd2bf130fe27b70dad27b6d8c61ad4a2462b4
Stanislav Klymoshenko <s.klymoshenko@gmail.com>
2022-02-13 21:04:20 +0100

    WIP components to show event history

---
+++ src/components/SpEvent/SpEventHistory.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="event-history"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "SpEventHistory",
+  setup() {
+    return {};
+  }
+});
+</script>
+
+<style scoped></style>
